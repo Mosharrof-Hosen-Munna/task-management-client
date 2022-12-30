446475b96cabefbf5065e834ed72a435fe11d606
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2022-12-30 20:06:36 +0600

    create all file structure

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import AddTask from "../Pages/AddTask/AddTask";
+import CompletedTask from "../Pages/CompletedTask/CompletedTask";
+import Login from "../Pages/Login/Login";
+import MyTask from "../Pages/MyTask/MyTask";
+import Signup from "../Pages/Signup/Signup";
 
 const Routes = createBrowserRouter([
   {
@@ -10,7 +14,23 @@ const Routes = createBrowserRouter([
         {
             path:'/',
             element:<AddTask/>
-        }
+        },
+        {
+            path:'/my-task',
+            element:<MyTask/>
+        },
+        {
+            path:'/completed-task',
+            element:<CompletedTask/>
+        },
+        {
+            path:'/login',
+            element:<Login/>
+        },
+        {
+            path:'/sign-up',
+            element:<Signup/>
+        },
     ]
     
   },
